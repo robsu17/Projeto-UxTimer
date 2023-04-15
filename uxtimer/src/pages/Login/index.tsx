9d7671d2c9829d28f6f19f8675ceacaf5878654b
robsu17 <robson.costa134@gmail.com>
2023-04-15 13:59:16 -0300

    Em progresso

---
+++ uxtimer/src/pages/Login/index.tsx
@@ -0,0 +1,15 @@
+
+import { Wrapper, Main, Header } from "./style";
+
+export default function Login() {
+    return (
+        <Wrapper>
+            <Main>
+                <Header>
+                    <h1>Timer</h1>
+                    <p>Seja você o senhor do seu próprio tempo</p>
+                </Header>
+            </Main>
+        </Wrapper>
+    )
+}
